bb8751c0d22ce94f2640c9660a5985b7ee05e9f2
dgomezs <mikrodj@gmail.com>
2017-01-25 09:00:13 +0100

    added import for Test module

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
+import {TestAngular2Module} from "test-angular-2-module";
 import { AppComponent } from "./app.component";
 
 @NgModule({
     declarations: [AppComponent],
     bootstrap: [AppComponent],
-    imports: [NativeScriptModule],
+    imports: [NativeScriptModule,TestAngular2Module],
     schemas: [NO_ERRORS_SCHEMA]
 })
 export class AppModule { }
