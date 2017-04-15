26677e663b461cd4d05a144cce7c5703401c36f3
dgomezs <mikrodj@gmail.com>
2017-04-15 19:32:13 +0200

    add angular directives to footer

--- app/footer.component.ts
+++ app/footer.component.ts
@@ -2,7 +2,7 @@ import {Component} from "@angular/core";
 
 @Component({
     selector: "my-footer",
-    template: "<StackLayout padding='20' horizontalAlignment='center' ><Label fontSize='20' style='text-align: center' text='This is the footer'></Label></StackLayout>",
+    template: "<StackLayout *ngIf='loaded' padding='20' horizontalAlignment='center' ><Label fontSize='20' style='text-align: center' text='This is the footer'></Label></StackLayout>",
 })
 export class FooterComponent {
 
